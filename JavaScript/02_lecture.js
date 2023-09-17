c8449183037e9eaaa7c73faed2efd4d0c2abbb73
Amit Kumar Sharma <138234619+As24045@users.noreply.github.com>
2023-09-17 06:40:15 +0000

    conversion of datatypes

---
+++ JavaScript/02_lecture.js
@@ -0,0 +1,22 @@
+//let score = "33 abs"
+//let score = null
+//let score = undefined
+// let score = true
+let score = false
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+//let value = 1
+//let value = 0
+//let value = ""
+let value = "Amit"
+
+let changeInBoolean = Boolean(value);
+console.log(changeInBoolean);
